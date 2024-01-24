8d09cbdc84f808908e42e167394186bdd985cc1c
Abhiforu12 <lu.abhi94@gmail.com>
2024-01-24 15:06:26 +0530

    navbar

--- abhiblog/settings.py
+++ abhiblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fi406f(&l3-fr98zjmp%xrjqw1oro_gw&zibx=-#r&d0yi5-2!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app','127.0.0.1']
 
 
 # Application definition
