302a4be595f48528e5e4a1461ea13d3dc504782c
Abhiforu12 <lu.abhi94@gmail.com>
2024-01-26 10:48:49 +0530

    done

--- abhiblog/settings.py
+++ abhiblog/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'static/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- article/models.py
+++ article/models.py
@@ -5,7 +5,12 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=500)
-    content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=10000)
+    img = models.ImageField( upload_to='article_images/',default= "dfault_imag.jpg")
+
+    pub_date = models.DateField( auto_now=True)
+
+
 
 
     def __str__(self):
