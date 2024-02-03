fe0d40a4256fd6f0a219038cebea8f6e9b7d93fb
Abhiforu12 <lu.abhi94@gmail.com>
2024-02-03 10:46:59 +0530

    'done'

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     path('profile/',profile,name='profile'),
+    path('profile/edit',edit_profile,name='edit_profile')
 
     
 ]
